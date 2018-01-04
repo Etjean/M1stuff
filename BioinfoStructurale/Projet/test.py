14a8a1fb25253d8d36ebdf10ac7422be0e251e45
Etienne from P7 <etienne_jean5@hotmail.fr>
2018-01-04 19:55:14 +0100

    sevrhb

--- BioinfoStructurale/Projet/test.py
+++ BioinfoStructurale/Projet/test.py
@@ -1,19 +1,5 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# http://uel.unisciel.fr/chimie/modelisation/modelisation_ch02/co/1-2_energie_de_deformation.html
-
-
-#################
-#    IMPORTS    #
-#################
+#!/usr/bin/env python3
 import numpy as np
-
-
-
-##########################
-#    DEFINE CONSTANTS    #
-##########################
 
 # Distance d'équilibre standard O-H (en A)
 L_EQ = 0.9572
@@ -27,19 +13,12 @@ K_ANGLE = 55.000
 DELTA = 0.0001
 
 
-
-###################
-#    FUNCTIONS    #
-###################
-
-#________________________________________________________________________________________________
-#________________________________________________________________________________________________
 def extract_coords(filepath):
     """ Retourne un vecteur avec les coordonnées xyz de chaque atome d'un fichier PDB. """
     coords = []
     with open(filepath, 'r') as filin:
         for line in filin:
-            if line[0:6].strip()=='ATOM':
+            if line[0:6].strip() == 'ATOM':
                 x = float(line[30:38].strip())
                 y = float(line[38:46].strip())
                 z = float(line[46:54].strip())
@@ -47,8 +26,6 @@ def extract_coords(filepath):
     return np.array(coords)
 
 
-#________________________________________________________________________________________________
-#________________________________________________________________________________________________
 def angle(vect1, vect2):
     """ Retourne l'angle theta entre les vecteurs 1 et 2. """
     dot = np.dot(vect1, vect2)
@@ -57,47 +34,33 @@ def angle(vect1, vect2):
     return np.arccos(dot/(norm1*norm2))
 
 
-#________________________________________________________________________________________________
-#________________________________________________________________________________________________
 def e_bond(l):
     """ Retourne l'énergie de liaison. """
     return (1/2)*K_BOND*(l-L_EQ)**2
 
 
-#________________________________________________________________________________________________
-#________________________________________________________________________________________________
 def e_angle(th):
     """ Retourne l'énergie d'angle. """
     return (1/2)*K_ANGLE*(th-TH_EQ)**2
 
 
-#________________________________________________________________________________________________
-#________________________________________________________________________________________________
 def e_potential(coords):
     """ Retourne l'énergie totale du champs de forces. """
     vect_OH1 = coords[0:3] - coords[3:6]
     vect_OH2 = coords[0:3] - coords[6:9]
-
     # Energies d'élongation de liaison
     l_OH1 = np.linalg.norm(vect_OH1)
     e_OH1 = e_bond(l_OH1)
-
     l_OH2 = np.linalg.norm(vect_OH2)
     e_OH2 = e_bond(l_OH2)
-
     # Energie de déformation angulaire
     th = angle(vect_OH1, vect_OH2)
     e_th = e_angle(th)
-
     # Energie d'interaction totale
     epot = e_OH1 + e_OH2 + e_th
-
     return epot
 
 
-
-#________________________________________________________________________________________________
-#________________________________________________________________________________________________
 def calc_gradient(coords):
     """ Calcul du gradient de l'énergie potentielle. """
     gradient = []
@@ -107,12 +70,9 @@ def calc_gradient(coords):
         coords_less_delta = coords.copy()
         coords_less_delta[i] -= DELTA
         gradient.append((e_potential(coords_plus_delta) - e_potential(coords_less_delta)) / (2*DELTA))
-
     return np.array(gradient)
 
 
-#________________________________________________________________________________________________
-#________________________________________________________________________________________________
 def line_search(coords, grad):
     """Renvoie le pas optimal pour la minimisation"""
     gold = (1+np.sqrt(5))/2
@@ -122,13 +82,13 @@ def line_search(coords, grad):
     epot_a = e_potential(coords - step_a*grad)
     epot_b = e_potential(coords - step_b*grad)
     epot_c = e_potential(coords - step_c*grad)
-    #initialisation de l'encadrement du minimum : recherche d'un encadrement adapté
+    # initialisation de l'encadrement du minimum : recherche d'un encadrement adapté
     while epot_a < epot_b or epot_c < epot_b:
         step_a *= 10
         step_c *= 10
         epot_a = e_potential(coords - step_a*grad)
         epot_c = e_potential(coords - step_c*grad)
-    #réduction de l'encadrement par la méthode du nombre d'or
+    # réduction de l'encadrement par la méthode du nombre d'or
     for _ in range(10):
         if (step_c-step_b) >= (step_b-step_a):
             step_d = (step_c+gold*step_b)/(gold+1)
@@ -146,77 +106,54 @@ def line_search(coords, grad):
                 step_b = step_d
             else:
                 step_a = step_d
-
         epot_a = e_potential(coords - step_a*grad)
         epot_b = e_potential(coords - step_b*grad)
         epot_c = e_potential(coords - step_c*grad)
-
     return step_b
 
 
-
-#________________________________________________________________________________________________
-#________________________________________________________________________________________________
-def steepest_descent(coords, threshold, max_iter):
+def steepest_descent(coords, threshold, max_iter, step=0.0002, ls=True, save=True):
     """" Méthode de Steepest Descent. """
-    n_iter = 1
-    grms = np.linalg.norm(calc_gradient(coords))
-
-    while(grms > threshold and n_iter <= max_iter):
+    n_iter = 0
+    coords_all = [coords]
+    grms = [np.linalg.norm(calc_gradient(coords))]
+    vpot = [e_potential(coords)]
+    while grms[-1] > threshold and n_iter < max_iter:
+        coords = coords_all[-1]
         grad = calc_gradient(coords)
-        step = line_search(coords, grad)
+        if ls:
+            step = line_search(coords, grad)
         coords = coords - step*grad
-        grms = np.linalg.norm(grad)
-        #print("Etape", n_iter, ": GRMS =", grms, end="\n")
+        # remplissage des listes
+        coords_all.append(coords)
+        grms.append(np.linalg.norm(calc_gradient(coords)))
+        vpot.append(e_potential(coords))
         n_iter += 1
-        time.sleep(0)
-
-    print("Nombre d'itérations : ", n_iter-1, "    GRMS : ", grms)
-
-    if(n_iter > max_iter):
+    print("Nombre d'itérations :", n_iter-1, "    GRMS : ", grms[-1])
+    if n_iter > max_iter:
         print("L'algorithme n'a pas convergé.")
+    if save:
+        with open('mini.txt', 'w') as filout:
+            filout.write("Step\tVpot\tGRMS\txO\tyO\tzO\txH1\tyH1\tzH1\txH2\tyH2\tzH2\n")
+            for step in range(n_iter):
+                filout.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(step, vpot[step], grms[step], coords_all[step][0], coords_all[step][1], coords_all[step][2], coords_all[step][3], coords_all[step][4], coords_all[step][5], coords_all[step][6], coords_all[step][7], coords_all[step][8]))
+    return coords_all[-1]
 
-    return coords
 
-
-
-##############
-#    MAIN    #
-##############
-
+# programme principal
 if __name__ == "__main__":
-
     # Coordonnées de départ : [O, H1, H2]
     coords = extract_coords('water.pdb')
     print("Array des coordonnées de départ :\n", coords, end='\n\n')
-
     # Energie potentielle de départ
     epot = e_potential(coords)
-    print("Energie potentielle de départ :\n", epot, end = '\n\n')
-
+    print("Energie potentielle de départ :\n", epot, end='\n\n')
     # Gradient de départ
     grad = calc_gradient(coords)
-    print("Gradient de départ :\n", grad, end = '\n\n')
-    
+    print("Gradient de départ :\n", grad, end='\n\n')
     # GRMS de départ
     grms = np.linalg.norm(coords)
     print("GRMS de départ :\n", grms, end='\n\n')
-    
-    mini = steepest_descent(coords, 0.00001, 100)
+    # Steepest descent
+    mini = steepest_descent(coords, 0.00001, 1000)
     print("Vecteur des coordonnées après minimisation :\n", mini, end='\n\n')
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
